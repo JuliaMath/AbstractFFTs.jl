abb4c51f5c25d7ef17632f828a733fcbf425bebd
Carsten Bauer <crstnbr@gmail.com>
2019-05-12 00:19:32 +0200

    add newline; <:Real -> <:Number

--- src/definitions.jl
+++ src/definitions.jl
@@ -399,7 +399,7 @@ end
 ##############################################################################
 
 
-struct Frequencies{T<:Real} <: AbstractVector{T}
+struct Frequencies{T<:Number} <: AbstractVector{T}
     nreal::Int
     n::Int
     multiplier::T
@@ -425,7 +425,7 @@ Frequencies object is an AbstractVector containing the frequency
 bin centers at every sample point. `fs` is the sample rate of the
 input signal.
 """
-fftfreq(n::Int, fs::Real=1) = Frequencies(((n-1) >> 1)+1, n, fs/n)
+fftfreq(n::Int, fs::Number=1) = Frequencies(((n-1) >> 1)+1, n, fs/n)
 
 """
     rfftfreq(n, fs=1)
@@ -434,7 +434,7 @@ Return discrete fourier transform sample frequencies for use with
 containing the frequency bin centers at every sample point. `fs`
 is the sample rate of the input signal.
 """
-rfftfreq(n::Int, fs::Real=1) = Frequencies((n >> 1)+1, (n >> 1)+1, fs/n)
+rfftfreq(n::Int, fs::Number=1) = Frequencies((n >> 1)+1, (n >> 1)+1, fs/n)
 
 fftshift(x::Frequencies) = (x.nreal-x.n:x.nreal-1)*x.multiplier
 
