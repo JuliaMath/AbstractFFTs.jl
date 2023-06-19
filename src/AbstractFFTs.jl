3a3f0e4ebc3a9ab81bfb655003759ffa9c6bd1bb
Vedant Puri <vedantpuri@gmail.com>
2023-06-19 09:47:40 -0400

    put chainrules core in a requires block, rm it from deps (#107)
    * put chainrules core in a requires block, rm it from deps
    
    * test conditional loading of CRC

--- src/AbstractFFTs.jl
+++ src/AbstractFFTs.jl
@@ -7,8 +7,16 @@ export fft, ifft, bfft, fft!, ifft!, bfft!,
 
 include("definitions.jl")
 
-if !isdefined(Base, :get_extension)
-    include("../ext/AbstractFFTsChainRulesCoreExt.jl")
+@static if !isdefined(Base, :get_extension)
+    import Requires
+end
+
+@static if !isdefined(Base, :get_extension)
+    function __init__()
+        Requires.@require ChainRulesCore = "d360d2e6-b24c-11e9-a2a3-2a2ae2dbcce4" begin
+            include("../ext/AbstractFFTsChainRulesCoreExt.jl")
+        end
+    end
 end
 
 end # module
