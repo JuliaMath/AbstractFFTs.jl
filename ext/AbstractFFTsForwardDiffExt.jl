a516bd0555a3fc8ac9b89902dc3fad9dd1b6dacf
Sheehan Olver <solver@mac.com>
2024-12-18 10:31:58 +0000

    Overload only _fftfloat

--- ext/AbstractFFTsForwardDiffExt.jl
+++ ext/AbstractFFTsForwardDiffExt.jl
@@ -7,11 +7,7 @@ import ForwardDiff: Dual
 import AbstractFFTs: Plan, mul!, dualplan, dual2array
 
 
-AbstractFFTs.complexfloat(x::AbstractArray{<:Dual}) = AbstractFFTs.complexfloat.(x)
-AbstractFFTs.complexfloat(d::Dual{T,V,N}) where {T,V,N} = convert(Dual{T,float(V),N}, d) + 0im
-
-AbstractFFTs.realfloat(x::AbstractArray{<:Dual}) = AbstractFFTs.realfloat.(x)
-AbstractFFTs.realfloat(d::Dual{T,V,N}) where {T,V,N} = convert(Dual{T,float(V),N}, d)
+AbstractFFTs._fftfloat(::Type{Dual{T,V,N}}) where {T,V,N} = Dual{T,AbstractFFTs._fftfloat(V),N}
 
 dual2array(x::Array{<:Dual{Tag,T}}) where {Tag,T} = reinterpret(reshape, T, x)
 dual2array(x::Array{<:Complex{<:Dual{Tag, T}}}) where {Tag,T} = complex.(dual2array(real(x)), dual2array(imag(x)))
